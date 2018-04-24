079549d55d747cf7399b00abebe656b5cf1d11de
Murray Coghill <murray.coghill@gmail.com>
2018-04-24 20:44:38 +0100

    Safeguard to only GIS with a valid image url

---
+++ test/GoogleImageSearcherTests.ts
@@ -0,0 +1,22 @@
+import 'mocha'
+import * as assert from 'assert'
+import { GoogleImageSearcher } from '../src/GoogleImageSearcher';
+
+describe('GoogleImageSearcher', () => {
+    describe('isImageURL', () => {
+        it('should return true if the URL ends with a valid extension', () => {
+            const validExtensions = ["png", "jpg", "jpeg", "bmp", "tiff"]
+            const url = `${randomString()}.${validExtensions[Math.floor(Math.random() * validExtensions.length)]}`
+
+            assert.equal(new GoogleImageSearcher().isImageURL(url), true)
+        })
+
+        it('should return true if the URL does not end with a valid extension', () => {
+            assert.equal(new GoogleImageSearcher().isImageURL(randomString()), false)
+        })
+    })
+})
+
+function randomString(): string {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
