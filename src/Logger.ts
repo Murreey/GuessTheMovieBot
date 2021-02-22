99bee9ac488cd42aef28a005e2df519addabf180
Murray Coghill <murray.coghill@gmail.com>
2021-02-22 17:13:41 +0000

    Logging

---
+++ src/Logger.ts
@@ -0,0 +1,59 @@
+import winston from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+export class Logger {
+    private static logger: winston.Logger
+
+    static setup({ console = LogLevel.INFO, file = LogLevel.INFO }: LogLevels = { console: LogLevel.INFO, file: LogLevel.INFO}): void {
+      this.logger = winston.createLogger({
+        format: winston.format.printf(info => `${info.level.toUpperCase().padStart(8, " ")}:  ${info.message}`),
+        transports: []
+      })
+
+      if(console) {
+        this.logger.add(new winston.transports.Console({ level: console }))
+      }
+
+      if(file) {
+        this.logger.add(new DailyRotateFile({
+          filename: '%DATE%.log',
+          dirname: 'logs',
+          level: file,
+          datePattern:'YYYY-MM-[week]-w',
+          format: winston.format.combine(
+              winston.format.timestamp({
+                  format: 'YYYY-MM-DD HH:mm:ss'
+              }),
+              winston.format.printf(info => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`)
+          )
+        }))
+      }
+    }
+
+    static get(logLevels?: LogLevels): winston.Logger {
+      if(!this.logger) this.setup(logLevels)
+      return this.logger
+    }
+
+    static error = (message: string) => Logger.get().log("error", message)
+    static warn = (message: string) => Logger.get().log("warn", message)
+    static info = (message: string) => Logger.get().log("info", message)
+    static http = (message: string) => Logger.get().log("http", message)
+    static verbose = (message: string) => Logger.get().log("verbose", message)
+    static debug = (message: string) => Logger.get().log("debug", message)
+    static silly = (message: string) => Logger.get().log("silly", message)
+}
+
+type LogLevels = {
+  console?: LogLevel,
+  file?: LogLevel
+}
+
+export enum LogLevel {
+  ERROR = "error",
+  WARN = "warn",
+  INFO = "info",
+  HTTP = "http",
+  VERBOSE = "verbose",
+  DEBUG = "debug",
+  SILLY = "silly"
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+import { Logger, LogLevel } from './Logger';
 import * as RedditBot from './RedditBot';
 import WinChecker from './WinChecker';
+import processWin from './WinProcessor'
 
-const bot = RedditBot.create({ debug: true, startFrom: "t1_gobjgub" })
+const bot = RedditBot.create({ debug: true, readOnly: true, startFrom: "t1_gobjgub" })
+Logger.setup({ file: null, console: LogLevel.SILLY })
 
 bot.fetchNewConfirmations().then(comments => {
-  comments.forEach(async comment => {
+  [comments[0]].forEach(async comment => {
     const valid = await WinChecker(bot).isValidWin(comment)
     console.log(`${comment.author.name}: ${comment.body.substr(0, 10)} (${comment.permalink})`)
     console.log(`- valid: ${valid}`)
+    await processWin(bot, comment)
   })
 })
