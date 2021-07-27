de3187b4635970938d8cd26176e83e114d8aac12
Murray Coghill <murray.coghill@gmail.com>
2021-07-27 19:44:18 +0100

    First pass at database manager

---
+++ scripts/create-database.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS users (
+  user_id INTEGER PRIMARY KEY,
+  username TEXT UNIQUE ON CONFLICT IGNORE
+);
+
+CREATE TABLE IF NOT EXISTS wins (
+  post_id TEXT PRIMARY KEY NOT NULL,
+  guesser_id INTEGER,
+  submitter_id INTEGER,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  FOREIGN KEY (guesser_id) REFERENCES users(user_id),
+  FOREIGN KEY (submitter_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS points (
+  post_id TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+  points INTEGER NOT NULL,
+  UNIQUE (post_id, user_id) ON CONFLICT REPLACE,
+  FOREIGN KEY (post_id) REFERENCES wins(post_id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+)
---
+++ src/scores/DatabaseManager.ts
@@ -0,0 +1,91 @@
+import fs from 'fs'
+import path from 'path'
+import sqlite3 from 'sqlite3'
+import { Database, open } from 'sqlite'
+import { Scores } from './Scores'
+
+const defaultTimeRange = {
+  from: '2000-01-01',
+  to: 'now'
+}
+
+export default async () => {
+  let db: Database<sqlite3.Database, sqlite3.Statement>
+
+  try {
+    db = await open({
+      filename: path.resolve(__dirname, '../../database.db'),
+      driver: sqlite3.cached.Database
+    })
+
+    await db.exec('PRAGMA journal_mode = WAL;')
+    await db.exec('PRAGMA foreign_keys = ON;')
+
+    await db.exec(fs.readFileSync(path.resolve(__dirname, '../../scripts/create-database.sql'), 'utf-8'))
+  } catch (ex) {
+    console.error(ex)
+    db = null
+  }
+
+  const getUserID = async (username: string): Promise<number> => {
+    await db.run(`INSERT OR IGNORE INTO users (username) VALUES (?)`, username)
+    return (await db.get(`SELECT user_id FROM users WHERE username = ?`, username)).user_id
+  }
+
+  return {
+    db,
+    saveScore: async (postID: string, guesser: string, submitter: string, scores: Scores) => {
+      const guesserID = await getUserID(guesser)
+      const submitterID = await getUserID(submitter)
+      await db.run(
+        `INSERT OR IGNORE INTO wins (post_id, guesser_id, submitter_id) VALUES (?, ?, ?)`,
+        postID, guesserID, submitterID
+      )
+      await db.run(
+        `INSERT OR IGNORE INTO points (post_id, user_id, points) VALUES (?, ?, ?)`,
+        postID, guesserID, scores.guesser
+      )
+      await db.run(
+        `INSERT OR IGNORE INTO points (post_id, user_id, points) VALUES (?, ?, ?)`,
+        postID, submitterID, scores.submitter
+      )
+    },
+    getUserScore: async (username: string, timeRange?: { from?: string, to?: string }): Promise<number> =>  {
+      timeRange = { ...defaultTimeRange, ...timeRange }
+
+      const data = await db.get(`
+        SELECT SUM(points.points) AS total FROM wins
+        INNER JOIN points ON wins.post_id = points.post_id
+        INNER JOIN users ON points.user_id = users.user_id
+        WHERE users.username = ?
+        AND wins.timestamp >= datetime(?)
+        AND wins.timestamp < datetime(?)`, username, timeRange.from, timeRange.to)
+
+      return data.total ?? 0
+    },
+    getUserSubmissionCount: async (username: string, timeRange?: { from?: string, to?: string }): Promise<number> =>  {
+      timeRange = { ...defaultTimeRange, ...timeRange }
+
+      const data = await db.get(`
+        SELECT COUNT(wins.post_id) as count FROM wins
+        LEFT JOIN users ON wins.submitter_id = users.user_id
+        WHERE users.username = ?
+        AND wins.timestamp >= datetime(?)
+        AND wins.timestamp < datetime(?)`, username, timeRange.from, timeRange.to)
+
+      return data.count ?? 0
+    },
+    getUserGuessCount: async (username: string, timeRange?: { from?: string, to?: string }): Promise<number> =>  {
+      timeRange = { ...defaultTimeRange, ...timeRange }
+
+      const data = await db.get(`
+        SELECT COUNT(wins.post_id) as count FROM wins
+        LEFT JOIN users ON wins.guesser_id = users.user_id
+        WHERE users.username = ?
+        AND wins.timestamp >= datetime(?)
+        AND wins.timestamp < datetime(?)`, username, timeRange.from, timeRange.to)
+
+      return data.count ?? 0
+    }
+  }
+}
