0efcf3afabe682cfdbbbc45fbb65bb5c230a5690
Murray Coghill <murray.coghill@gmail.com>
2021-08-27 19:05:26 +0100

    Script for quick score checking

---
+++ scripts/fetch-scores.ts
@@ -0,0 +1,53 @@
+import yargs from 'yargs'
+import { Logger, LogLevel } from '../src/Logger';
+import * as RedditBot from '../src/RedditBot';
+import ScoreManager from '../src/scores/ScoreManager';
+import DatabaseManager from '../src/scores/DatabaseManager';
+import { TimeRange } from '../src/types';
+
+const userScore = async (args) => {
+  let range: Partial<TimeRange> = {}
+  if(args.from) range.from = new Date(args.from)
+  if(args.to) range.to = new Date(args.to)
+
+  const db = await DatabaseManager()
+  const score = await db.getUserScore(args.username, range)
+
+  console.log(`  ${args.username} has ${score} points${range.from ? ' from ' + range.from.toUTCString() : ''}${range.to ? ' to ' + range.to.toUTCString() : ''}`)
+  console.log('')
+}
+
+const highScores = async (args) =>  {
+  const range: TimeRange = {
+    from: new Date(args.from),
+    to: new Date(args.to)
+  }
+
+  const db = await DatabaseManager()
+  const scores = await db.getHighScores(range)
+
+  console.log(`  High scores from ${range.from.toUTCString()} to ${range.to.toUTCString()}`)
+  console.log('  Top Scores:')
+  scores.scores.forEach((score, index) => console.log(`  - ${index+1}. ${score.username}: ${score.score}`))
+  console.log('')
+  console.log('  Top Guessers:')
+  scores.guessers.forEach((score, index) => console.log(`  - ${index+1}. ${score.username}: ${score.score}`))
+  console.log('')
+  console.log('  Top Submitters:')
+  scores.submitters.forEach((score, index) => console.log(`  - ${index+1}. ${score.username}: ${score.score}`))
+  console.log('')
+}
+
+const args = yargs(process.argv.slice(2))
+  .command('user <username> [from] [to]', `check user's points total`, {}, userScore)
+  .command('highscores <from> <to>', `fetch high scores`, {}, highScores)
+  .option('log-level', {alias: 'll', choices: Object.values(LogLevel), default: LogLevel.INFO})
+  .option('read-only', {type: 'boolean', alias: 'r', description: 'block the bot from making real changes to reddit'})
+  .option('debug-requests', {type: 'boolean', alias: 'd', description: 'display snoowrap request debugging'})
+  .demandCommand(1)
+  .help()
+  .argv
+
+Logger.setup({ file: LogLevel.INFO, console: args['log-level'] })
+
+// const bot = RedditBot.create({ debug: args['debug-requests'], readOnly: args['read-only'] });
