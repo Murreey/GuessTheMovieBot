3a53afd7340fee1d1f65171b52105df88ea02a84
Murray Coghill <murray.coghill@gmail.com>
2021-08-30 16:46:13 +0100

    Make import script only keep latest total

--- scripts/import-flatfiles.ts
+++ scripts/import-flatfiles.ts
@@ -5,12 +5,18 @@ import DatabaseManager from '../src/scores/DatabaseManager'
 
 (async () => {
     const scoreDir = path.resolve(__dirname, '../scores')
+    const months = 'janfebmaraprmayjunjulaugsepoctnovdec'.match(/.{3}/g)
     const scores: {
         [username: string]: {
             total: number, guesses: number, submissions: number
         }
     } = fs
         .readdirSync(scoreDir)
+        .filter(file => /^\d{4}-[a-z]{3}/.test(file))
+        .sort((a, b) => {
+            const [x, y] = [a, b].map(x => x.split('.')?.[0]?.split('-'))
+            return parseInt(x[0]) - parseInt(y[0]) || months.indexOf(x[1]) - months.indexOf(y[1])
+        })
         .map(file => JSON.parse(fs.readFileSync(path.resolve(scoreDir, file), 'utf-8')))
         .reduce((allScores, fileData) => {
             for(const user in fileData) {
@@ -25,9 +31,7 @@ import DatabaseManager from '../src/scores/DatabaseManager'
                 } else {
                     allScores[user].guesses = (allScores[user]?.guesses || 0) + (scores.guesses || 0)
                     allScores[user].submissions = (allScores[user]?.submissions || 0) + (scores.submissions || 0)
-                    if(scores.total > (allScores[user]?.total || 0)) {
-                        allScores[user].total = scores.total
-                    }
+                    allScores[user].total = scores?.total || 0
                 }
             }
 
@@ -40,6 +44,6 @@ import DatabaseManager from '../src/scores/DatabaseManager'
        const score = scores[user]
        console.log(`Importing ${user}...`)
        const userID = await db.getUserID(user)
-       await db.db.run(`INSERT OR IGNORE INTO legacy_imports (user_id, points, guesses, submissions) VALUES (?, ?, ?, ?)`, userID, score.total, score.guesses, score.submissions)
+       await db.db.run(`INSERT OR REPLACE INTO legacy_imports (user_id, points, guesses, submissions) VALUES (?, ?, ?, ?)`, userID, score.total, score.guesses, score.submissions)
     }
 })()
